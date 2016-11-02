6e56c3ba319cc0f5bb98ee9febd0609659fbebd6
Yaohai Zheng <yaozheng@microsoft.com>
2016-11-02 15:23:06 +0800

    Fix found issues.

--- Lesson1/gulpfile.js
+++ Lesson1/gulpfile.js
@@ -1,4 +1,8 @@
-﻿require('gulp-common')(require('gulp'), 'arduino-esp8266-huzzah', {
+﻿/*
+* IoT Hub Adafruit Feather HUZZAH ESP8266 - Microsoft Sample Code - Copyright (c) 2016 - Licensed MIT
+*/
+
+require('gulp-common')(require('gulp'), 'arduino-esp8266-huzzah', {
   appName: 'lesson-1',
   configTemplate: {},
   configPostfix: "huzzah",
